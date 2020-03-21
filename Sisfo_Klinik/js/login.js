bb2f7cde645ef7d85f8d117aed0ea0b622f192e7
Gunawan Nur Ahmad <54525132+Gunawanart@users.noreply.github.com>
2020-03-21 21:10:11 +0700

    menambah js file

---
+++ Sisfo_Klinik/js/login.js
@@ -0,0 +1,12 @@
+const a = document.querySelectorAll(".teks");
+const b = document.getElementsByClassName("1");
+console.log(b[0].innerHTML);
+
+let i = 0;
+setInterval(function() {
+  if (i > 4) {
+    i = 0;
+  }
+  a[0].innerHTML = b[i].innerHTML;
+  i += 1;
+}, 5000);
